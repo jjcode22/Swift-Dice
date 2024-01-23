a605739531fadd9b9cfba4ea30fbf7e23d21e481
jjcode22 <jjmcoding31@gmail.com>
2024-01-23 19:55:21 +0530

    landscape support

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let dices = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
+        diceImageView1.image = dices[Int.random(in: 0...5)]
+        diceImageView2.image = dices[Int.random(in: 0...5)]
+        
     }
-
-
 }
 
